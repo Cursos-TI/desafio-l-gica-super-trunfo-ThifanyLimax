08f1f895e211f2ad1fc4c75def2744beb7f2b04f
ThifanyLimax <Thicasagaz24@gmail.com>
2025-05-13 22:47:05 +0000

    corrigi erro do scanf

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    // Variáveis da Carta 1
+    // Dados da Carta 1
     char estado1;
     char codigo1[4];
     char nomeCidade1[50];
@@ -10,7 +10,7 @@ int main() {
     float pib1;
     int pontosTuristicos1;
 
-    // Variáveis da Carta 2
+    // Dados da Carta 2
     char estado2;
     char codigo2[4];
     char nomeCidade2[50];
@@ -19,7 +19,7 @@ int main() {
     float pib2;
     int pontosTuristicos2;
 
-    // Entrada de dados da Carta 1
+    // Entrada dos dados da Carta 1
     printf("Informe os dados da Carta 1:\n");
     printf("Estado (letra de A a H): ");
     scanf(" %c", &estado1);
@@ -42,7 +42,7 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontosTuristicos1);
 
-    // Entrada de dados da Carta 2
+    // Entrada dos dados da Carta 2
     printf("\nInforme os dados da Carta 2:\n");
     printf("Estado (letra de A a H): ");
     scanf(" %c", &estado2);
@@ -65,7 +65,7 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontosTuristicos2);
 
-    // Exibição da Carta 1
+    // Exibição das duas cartas
     printf("\nCarta 1:\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
@@ -73,9 +73,8 @@ int main() {
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
-    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Pontos Turísticos: %d\n", pontosTuristicos1);
 
-    // Exibição da Carta 2
     printf("\nCarta 2:\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
@@ -83,7 +82,19 @@ int main() {
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
-    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Pontos Turísticos: %d\n", pontosTuristicos2);
+
+    // Comparação por população
+    printf("\nComparação de cartas (Atributo: População):\n");
+    printf("Carta 1 - %s: %d habitantes\n", nomeCidade1, populacao1);
+    printf("Carta 2 - %s: %d habitantes\n", nomeCidade2, populacao2);
+
+    if (populacao1 > populacao2) {
+        printf("Resultado: Carta 1 (%s) venceu!\n", nomeCidade1);
+    } else if (populacao2 > populacao1) {
+        printf("Resultado: Carta 2 (%s) venceu!\n", nomeCidade2);
+    } else {
+        printf("Resultado: Empate! As duas cidades têm a mesma população.\n");
+    }
 
     return 0;
-}
